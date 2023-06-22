ace70eb1f3017f2289b78841cbb06778551ef2ca
zhaoyutian <yutian_zhao@foxmail.com>
2023-06-23 01:26:18 +0900

    add log, tensorboard, dropout

--- prediction_model.py
+++ prediction_model.py
@@ -1,21 +1,26 @@
+import logging
 import torch
 from logloader import AIWolfDataset
 from torch.utils.data import DataLoader
 from torch import nn
 import random
 import numpy as np
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
 
 class CNNLSTM(nn.Module):
-    def __init__(self, in_channel=8, hid_dim=800):
+    def __init__(self, in_channel=8, hid_dim=800, dropout=0.2):
         super().__init__()
         self.cnn = nn.Sequential(
             nn.BatchNorm2d(in_channel),
             nn.Conv2d(in_channel, 32, 3, stride=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
+            nn.Dropout(dropout),
             nn.Conv2d(32, 64, 3, stride=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
+            nn.Dropout(dropout),
         )
         self.flat_linear = nn.Sequential(
             nn.Flatten(),
@@ -25,6 +30,7 @@ class CNNLSTM(nn.Module):
         self.linear = nn.Sequential(
             nn.Linear(hid_dim, hid_dim),
             nn.ReLU(),
+            nn.Dropout(dropout),
             nn.Linear(hid_dim, 15),
         )
 
@@ -58,7 +64,7 @@ class EarlyStopper:
                 return True
         return False
 
-def train_loop(dataloader, model, loss_fn, optimizer, device):
+def train_loop(dataloader, model, loss_fn, optimizer, device, writer, epoch):
     size = len(dataloader.dataset)
     # Set the model to training mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
@@ -77,17 +83,24 @@ def train_loop(dataloader, model, loss_fn, optimizer, device):
 
         if batch % 100 == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-    return loss
+            accuracy = (torch.round(pred) == y).type(torch.float).sum()/torch.numel(y)
+            logging.info(f"loss: {loss:>7f}  accuracy: {accuracy:>7f}  [{current:>5d}/{size:>5d}]")
+            
+            writer.add_scalar('Loss/Train', loss,  epoch * len(dataloader) + 
+                                batch)
+            writer.add_scalar('Accuracy/Train', accuracy, 
+                              epoch * len(dataloader) + batch)
+
+    return loss, accuracy
 
 
-def test_loop(dataloader, model, loss_fn, device):
+def test_loop(dataloader, model, loss_fn, device, writer=None, epoch=None, mode='valid'):
     # Set the model to evaluation mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    test_loss, correct = 0, 0
+    test_loss, accuracy = 0, 0
 
     # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
     # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
@@ -97,14 +110,22 @@ def test_loop(dataloader, model, loss_fn, device):
             y = y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            correct += (torch.round(pred) == y).type(torch.float).sum()/torch.numel(y)
+            accuracy += (torch.round(pred) == y).type(torch.float).sum()/torch.numel(y)
 
     test_loss /= num_batches
-    correct /= num_batches
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    return test_loss
+    accuracy /= num_batches
+    logging.info(f"{mode} error: accuracy: {(100*accuracy):>0.1f}%, avg loss: {test_loss:>8f} \n")
+    if mode == 'valid':
+        writer.add_scalar('Loss/Valid', test_loss, epoch * len(dataloader))
+        writer.add_scalar('Accuracy/Valid', accuracy, epoch * len(dataloader))
+    return test_loss, accuracy
 
 if __name__ == '__main__':
+    start_time = datetime.now()
+    writer = SummaryWriter(filename_suffix='CNNLSTM')
+    
+    logging.basicConfig(filename='logs/{}.log'.format(start_time.strftime('%m%d%H%M%S')), format='%(asctime)s [%(levelname)s] %(message)s', encoding='utf-8', level=logging.INFO)
+
     device = (
         "cuda"
         if torch.cuda.is_available()
@@ -112,7 +133,7 @@ if __name__ == '__main__':
         if torch.backends.mps.is_available()
         else "cpu"
     )
-    print(f"Using {device} device")
+    logging.info(f"Using {device} device")
 
     dataset_dir = "gat2017log15.pt"
     aiwolf_dataset = AIWolfDataset(dataset_dir)
@@ -158,14 +179,17 @@ if __name__ == '__main__':
     train_dataset = torch.utils.data.Subset(aiwolf_dataset, train_indices)
     test_dataset = torch.utils.data.Subset(aiwolf_dataset, test_indices)
     valid_dataset = torch.utils.data.Subset(aiwolf_dataset, valid_indices)
-    print("training dataset len: {}; valid dataset: {}, testing dataset len: {}".format(len(train_dataset), len(valid_dataset), len(test_dataset)))
+    logging.info("training dataset len: {}; valid dataset: {}, testing dataset len: {}".format(len(train_dataset), len(valid_dataset), len(test_dataset)))
 
     model = CNNLSTM().to(device)
+    writer.add_graph(model, train_dataset[0][0].unsqueeze(0).to(device))
     learning_rate = 1e-4
     batch_size = 64
     epochs = 100
+    weight_decay = 1
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, 
+                                  weight_decay=weight_decay)
     stopper = EarlyStopper(patience=10, min_delta=-0.001)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
@@ -173,13 +197,19 @@ if __name__ == '__main__':
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
 
     for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(train_dataloader, model, loss_fn, optimizer, device)
-        valid_loss = test_loop(valid_dataloader, model, loss_fn, device)
+        logging.info(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer, device, writer,
+                    epoch=t, )
+        valid_loss, valid_acc = test_loop(valid_dataloader, model, loss_fn, device, writer, epoch=t, mode='valid')
         if stopper.early_stop(valid_loss):
             break
-    test_loss = test_loop(test_dataloader, model, loss_fn, device)
-    print("Test loss: {}".format(test_loss))
+    test_loss, test_acc = test_loop(test_dataloader, model, loss_fn, device, mode='test')
+    writer.add_hparams({'lr': learning_rate, 'bsize': batch_size, "wdecay":weight_decay}, {'Loss/Test': test_loss, 'Accuracy/Test': test_acc})
 
-    torch.save(model, 'overfit.pt')
+    end_time = datetime.now()
+    duration = end_time - start_time
+    logging.info("Duration: {}".format(str(duration)))
 
+    torch.save(model, 'CNNLSTM_{}.pt'.format(start_time.strftime('%m%d%H%M%S')))
+    writer.flush()
+    writer.close()
